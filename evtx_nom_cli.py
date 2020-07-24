fa0c563c2e98d1f461b52ca8c2f67a92c9078735
tomrade <tommbray@gmail.com>
2020-07-24 16:16:48 +0000

    first steps on the road to modular

--- evtx_nom_cli.py
+++ evtx_nom_cli.py
@@ -1,19 +1,28 @@
-from lib.nom import prep_es, ingest_file
+from lib.nom import elastic_nom
 import json
 import argparse
 import os
 
 # TODO args etc etc
 
+# Open Config File
+with open('config.json','r') as conf_file:
+    config = json.load(conf_file)
+
 # Grab All the files
 target_list = []
-for root,d_names,f_names in os.walk('sample_logs'):
-    for f in f_names:
-        if f.endswith('.evtx'):
-            target_list.append(os.path.join(root, f))
+for path in config['inputs']['directory']['paths']:
+    for root,d_names,f_names in os.walk(path):
+        for f in f_names:
+            if f.endswith('.evtx'):
+                target_list.append(os.path.join(root, f))
 print(target_list)
-# Set Template
-prep_es()
-# Ingest Files
-for target in target_list:
-    ingest_file(target)
+
+# Open Plugins
+for output in config['outputs']:
+    if output == "elasticsearch":
+        #es output
+        nom = elastic_nom(config['outputs'][output])
+        # Ingest Files
+        for target in target_list:
+            nom.ingest_file(target)
--- lib/nom.py
+++ lib/nom.py
@@ -3,13 +3,60 @@ import json
 import datetime
 from elasticsearch import helpers, Elasticsearch
 
-# This file is parsing the evtx file and ingesting into elasticsearch
-
-
-# Parse Date to Python object ISO 8601/ RFC3339
-def parse_date(datestring):
-    output = datetime.datetime.fromisoformat(datestring.replace('Z','+00:00'))
-    return output
+# This file is parsing the evtx file and any default modules
+class elastic_nom():
+    def __init__(self,config):
+        self.name = "elasticseach ingest"
+        self.es_host = config['es_host']
+        self.es_port = config['es_port']
+        self.es_index = config['es_index']
+        self.security = config['security']
+        self.es_user = config['es_user']
+        self.es_pass = config['es_pass']
+        self.es_api_key = config['es_api_key']
+        self.prep_es()
+    def prep_es(self):
+        # connect to es
+        es = Elasticsearch()
+        # set/reset template
+        with open("es_stuff/index-template.json","r") as t_file:
+            template = json.load(t_file)
+        es.indices.put_template(name="evtx-nom",body=template)
+        return es
+    def ingest_file(self,filename):
+        # Process 1 file ah ah ah
+        print("Starting work on target {}".format(filename))
+        es = Elasticsearch()
+        start = datetime.datetime.utcnow()
+        errors = 0
+        done = 0
+        for ok, action in helpers.streaming_bulk(
+            client=es, actions=self.prepare_actions(filename)
+        ):
+            if not ok:
+                errors += 1
+            done += 1
+        end = datetime.datetime.utcnow()
+        duration = end - start
+        print("Finished Processing {} in {} seconds. ingested {} out of {} events".format(filename,duration.seconds,done - errors, done))
+        return {'errors' : errors, 'done' : done}
+    def prepare_actions(self,filename):
+        # This method is a wrapper around the base nom method to add each event as a bulk index action
+        for event in nom_file(filename):
+            source = {
+                '@timestamp' : self.parse_date(event['timecreated']['systemtime']),
+                'winlog' : event,
+                'os' : {"platform" : "windows"}
+            }
+            action = {
+                '_index': 'evtx_nom',
+                '_source': source
+            }
+            yield action
+    def parse_date(self,datestring):
+        # Parse Date to Python object ISO 8601/ RFC3339
+        output = datetime.datetime.fromisoformat(datestring.replace('Z','+00:00'))
+        return output
 
 
 # Get values form EVTX-RS json which may be attributes from XML land 
@@ -46,7 +93,6 @@ def get_section(item):
 
 # iterator from evtx-rs You can use this standalone if you want (ie for splunk)
 def nom_file(filename):
-    actions = []
     parser = PyEvtxParser(filename)
     # Open Records
     for record in parser.records_json():
@@ -57,41 +103,4 @@ def nom_file(filename):
         event.update(get_section(data['Event']['System']))
         if data['Event'].get('EventData'):
             event['event_data'] = get_section(data['Event']['EventData'])
-        source = {
-            '@timestamp' : parse_date(event['timecreated']['systemtime']),
-            'winlog' : event,
-            'os' : {"platform" : "windows"}
-        }
-        #print(json.dumps(source,indent=4))
-        action = {
-            '_index': 'evtx_nom',
-            '_source': source
-        }
-        yield action
-
-def prep_es():
-    # connect to es
-    es = Elasticsearch()
-    # set/reset template
-    with open("es_stuff/index-template.json","r") as t_file:
-        template = json.load(t_file)
-    es.indices.put_template(name="evtx-nom",body=template)
-    return es
-
-# elasticsearch ingestorator
-def ingest_file(filename):
-    print("Starting work on target {}".format(filename))
-    es = Elasticsearch()
-    start = datetime.datetime.utcnow()
-    errors = 0
-    done = 0
-    for ok, action in helpers.streaming_bulk(
-        client=es, actions=nom_file(filename)
-    ):
-        if not ok:
-            errors += 1
-        done += 1
-    end = datetime.datetime.utcnow()
-    duration = end - start
-    print("Finished Processing {} in {} seconds. ingested {} out of {} events".format(filename,duration.seconds,done - errors, done))
-    return {'errors' : errors, 'done' : done}
+        yield event
